fa973a39756c3e4c51d91ebccebd385ef2c97450
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-30 12:10:41 +0900

    add Apple (2828) in baekjoon

---
+++ src/baekjoon/greedy/apple.java
@@ -0,0 +1,52 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class apple {
+
+    //2828
+    public void greedy() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());  //스크린
+        int m = Integer.parseInt(st.nextToken());  //바구니 크기
+        int apple = Integer.parseInt(br.readLine());  //사과 개수
+
+        int left = 1;  //바구니 왼쪽 좌표
+        int right = m;  //바구니 오른쪽 좌표
+        int sum = 0;  //이동 거리
+
+        for(int i = 0; i < apple; i++) {
+            int where = Integer.parseInt(br.readLine());
+
+            //바구니 안에 떨어지는 경우
+            if(left <= where && where <= right) {
+                continue;
+            }
+
+            //바구니 왼쪽에 가깝게 떨어지는 경우
+//            if ((left - where) < (right - where)) {
+//                sum += (left - where);
+//                right -= (left - where);
+//                left = where;
+//            }
+            if(left > where) {
+                sum += (left - where);
+                right -= (left - where);
+                left = where;
+            }
+
+            //바구니 오른쪽에 가깝게 떨어지는 경우
+            else{
+                sum += (where - right);
+                left += (where - right);
+                right = where;
+            }
+        }
+        System.out.println(sum);
+    }
+}
