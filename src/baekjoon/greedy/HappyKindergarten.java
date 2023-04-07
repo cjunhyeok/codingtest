da1ddd20320d80e3f5ff1919253e30fe4455991a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-07 16:38:32 +0900

    add HappyKindergarten (13164) in baekjoon

---
+++ src/baekjoon/greedy/HappyKindergarten.java
@@ -0,0 +1,42 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class HappyKindergarten {
+
+    // 13164
+    public void greedy() throws IOException {
+        int n, k, result = 0;
+        int[] arr;
+        List<Integer> list = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split(" ");
+        n = Integer.parseInt(s1[0]);
+        k = Integer.parseInt(s1[1]);
+        arr = new int[n];
+        String[] s = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 1; i < n; i++) {
+            list.add(arr[i] - arr[i - 1]);
+        }
+        Collections.sort(list);
+        for (int i = 0; i < n - k; i++) {
+            result+=list.get(i);
+        }
+
+        System.out.println(result);
+    }
+}
