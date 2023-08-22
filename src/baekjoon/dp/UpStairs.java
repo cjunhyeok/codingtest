260fde9dad2abbeb6777fdbf93761f79824cf3a9
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-22 10:37:05 +0900

    Add UpStairs 2579 in dp

---
+++ src/baekjoon/dp/UpStairs.java
@@ -0,0 +1,30 @@
+package baekjoon.dp;
+
+import java.util.Scanner;
+
+public class UpStairs {
+
+    // 2579
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] stair = new int[301];
+        int[] dp = new int[301];
+        for (int i = 1; i <= N; i++) {
+            stair[i] = sc.nextInt();
+        }
+
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+        dp[3] = Math.max(stair[1], stair[2]) + stair[3];
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 3] + stair[i - 1], dp[i - 2]) + stair[i];
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
