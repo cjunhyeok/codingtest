2f03c2ebda405451c615dcfa256af81061f4cd06
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-03-22 20:03:39 +0900

    Add FindBehindBigNumber in programmers kit

---
+++ src/programmers/stack/FindBehindBigNumber.java
@@ -0,0 +1,27 @@
+package programmers.stack;
+
+import java.util.*;
+
+public class FindBehindBigNumber {
+
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            while (!s.isEmpty()) {
+                if (s.peek() > numbers[i]) {
+                    answer[i] = s.peek();
+                    break;
+                } else {
+                    s.pop();
+                }
+            }
+            if (s.isEmpty()) {
+                answer[i] = -1;
+            }
+            s.push(numbers[i]);
+        }
+        return answer;
+    }
+}
