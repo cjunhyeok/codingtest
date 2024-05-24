5a694bbfb527cf858c6554a0363e4a417a03e2b6
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-24 21:31:10 +0900

    Add CockProblem in lv.1

---
+++ src/programmers/lv1/CockProblem.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+public class CockProblem {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 1, 20));
+    }
+
+    public static int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n / a > 0) {
+            answer += (n / a) * b;
+            n = (n / a) * b + n % a;
+        }
+
+        return answer;
+    }
+}
