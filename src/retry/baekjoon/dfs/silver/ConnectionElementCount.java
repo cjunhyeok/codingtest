107753d252fd921788907e8be677a1ed702ed98a
Choi jun hyoek <chlwnsgur1214@naver.com>
2025-02-18 13:21:40 +0900

    Add ConnectionElementCount in dfs

---
+++ src/retry/baekjoon/dfs/silver/ConnectionElementCount.java
@@ -0,0 +1,55 @@
+package retry.baekjoon.dfs.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ConnectionElementCount {
+
+    static int N, M;
+    static int[][] connections;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        connections = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p1 = Integer.parseInt(st.nextToken());
+            int p2 = Integer.parseInt(st.nextToken());
+
+            connections[p1][p2] = connections[p2][p1] = 1;
+        }
+
+        Long count = 0L;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfsMethod(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static void dfsMethod(int start) {
+        if (visited[start]) {
+            return;
+        }
+
+        visited[start] = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && connections[start][i] == 1) {
+                dfsMethod(i);
+            }
+        }
+    }
+}
