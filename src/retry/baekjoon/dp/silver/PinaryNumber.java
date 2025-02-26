15b01fd7d9fcf8d591e1ce53a70d4525fa77a9ac
Choi jun hyoek <chlwnsgur1214@naver.com>
2025-02-26 21:21:52 +0900

    Add PinaryNumber in dp

---
+++ src/retry/baekjoon/dp/silver/PinaryNumber.java
@@ -0,0 +1,25 @@
+package retry.baekjoon.dp.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PinaryNumber {
+
+    // 2193
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Long[] dp = new Long[91];
+
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 2L;
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
